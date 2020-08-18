95a0c82f2ac5fea123bed3ac06c333e1ec45b8e7
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-08-18 16:15:07 -0400

    Add Fuel bar to ResourceView

--- src/ResourceView.js
+++ src/ResourceView.js
@@ -31,17 +31,79 @@ class ResourceView {
 				this.container.addChild(credits)
 				verticalOffset += creditsIcon.height
 			}
+			if (obj.fuel) {
+				let current = obj.fuel.current
+				let max = obj.fuel.max
+				let percentage = current/max
+
+				let graphic = new PIXI.Graphics()
+				let height = width/4
+
+				if (!obj.fuel.elementDistribution) {
+					obj.fuel.elementDistribution = [1] //Fuel distribution - decimal percentage that each element is of total fuel.
+				}
+
+				//renderWidths - width to render each element at in fuel breakdown.
+				let renderWidths = []
+				for (let i=0;i<obj.fuel.elementDistribution.length;i++) {
+					let current = obj.fuel.elementDistribution[i]
+					if (i) {
+						current += renderWidths[i - 1]
+					}
+					if (current > 1.0001) {throw "Sum of obj.fuel.elementDistribution should not be greater than 1. "}
+					renderWidths.push(current)
+				}
+
+				console.log(renderWidths)
+				//Draw rectangles for each element, overlapping, to show a distribution of per element power
+				let elementColors = [0xBB00FF, 0x2991DB, 0xd4b22c, 0xde7147, 0x6d4ade, 0x696cbc]
+				for (let i=renderWidths.length - 1;i>=0;i--) {
+					graphic.beginFill(elementColors[i])
+					let renderPercentage = percentage * renderWidths[i]
+					graphic.drawRoundedRect(width * (1-renderPercentage), verticalOffset, width * renderPercentage, height, height/4)
+					graphic.endFill()
+
+				}
+
+				console.log(graphic)
+
+
+				//We'll add text showing the remaining fuel. If above 50%, show in tank, else to left.
+				let text = new PIXI.Text(current, {
+					font: width/3 + "px Arial",
+					fill: "white"
+				});
+
+				text.y = verticalOffset + graphic.height / 2
+				text.anchor.y = 0.5
+
+				if (percentage > 0.5) {
+					text.x = width - text.width - graphic.width / 3
+				}
+				else {
+					text.x = width - graphic.width - text.width - width / 20 // width/20 for spacing.
+				}
+
+				//Draw bounding box. Do this AFTER text added to avoid messing with width calculations.
+				graphic.beginFill(0xFFFFFF, 0.2)
+				graphic.drawRoundedRect(0, verticalOffset, width, height, height/4)
+				graphic.endFill()
+
+				this.container.addChild(graphic)
+				this.container.addChild(text)
+				verticalOffset += height
+			}
 			if (obj.battery) {
+				let current = obj.battery[0]
+				let max = obj.battery[1]
+				let percentage = current/max
+
 				let batteryIcon = new PIXI.Sprite(resources.battery.texture)
 				batteryIcon.y = verticalOffset
 				let downsize = batteryIcon.width / width
 				batteryIcon.width /= downsize
 				batteryIcon.height /= downsize
 				batteryIcon.height /= 1.8 //The battery is a bit tall. Shrink it.
-
-				let current = obj.battery[0]
-				let max = obj.battery[1]
-				let percentage = current/max
 
 				batteryIcon.width *= percentage
 				batteryIcon.x = width - batteryIcon.width
@@ -68,9 +130,6 @@ class ResourceView {
 				this.container.addChild(batteryIcon)
 				this.container.addChild(text)
 				verticalOffset += batteryIcon.height
-			}
-			if (obj.fuel) {
-				//TODO: Render fuel.
 			}
 			if (obj.elements) {
 
@@ -115,6 +174,11 @@ a = new ResourceView()
 a.render({
 	credits: 500,
 	battery: [100, 150],
-	elements: [400]
+	elements: [400, 250, 133, 112, 76, 22],
+	fuel: {
+		current: 100,
+		max: 200,
+		elementDistribution: [0.6, 0.2, 0.1, 0.05, 0.03, 0.02]
+	}
 })
 */
