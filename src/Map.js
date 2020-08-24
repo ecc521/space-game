07eeb3061f7c3968b5656b303125b7943621d318
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-08-24 00:51:15 -0400

    Add map zoom. Significant touch ups needed.

--- src/Map.js
+++ src/Map.js
@@ -7,44 +7,107 @@ class Map {
 		this.height = config.height
 
 		this.container = new PIXI.Container()
+		this.container.zIndex = -1
+		console.log(this.container)
 
 		this.backgroundOptions = config.backgroundOptions
 
+		//Bounds in which map will be displayed
 		this.bounds = {
-			xmin: -this.width + app.renderer.width/app.renderer.resolution,
+			xmin: app.renderer.width/app.renderer.resolution,
 			xmax: 0,
-			ymin: -this.height + app.renderer.height/app.renderer.resolution,
+			ymin: app.renderer.height/app.renderer.resolution,
 			ymax: 0,
 		}
-		Object.assign(this.bounds, config.bounds || {})
-		Object.assign(this.bounds, {
-			startx: (this.bounds.xmin + this.bounds.xmax)/2,
-			starty: (this.bounds.ymin + this.bounds.ymax)/2
-		})
+
 		Object.assign(this.bounds, config.bounds || {})
 
-		this.container.x = this.bounds.startx
-		this.container.y = this.bounds.starty
+
+		this.container.x = 0
+		this.container.y = 0
 
 		if (config.backgroundOptions) {
 			this.background = new PIXI.Sprite(resources[this.backgroundOptions[Math.floor(Math.random() * this.backgroundOptions.length)]].texture)
 			this.background.width = this.width
 			this.background.height = this.height
 			this.container.addChild(this.background)
+			this.container.zIndex = -1
 		}
 
 		//Make map draggable
 		this.container.interactive = true
+
+		let translate = (function translate(x, y) {
+			this.container.x = this.container.x + x;
+			this.container.y = this.container.y + y;
+		}).bind(this)
+
+		let scale = 1
+		let zoom = (function zoom(factorChange = 0, centerX = 0, centerY = 0) {
+			//Note: centerX and centerY are page coordinates. They are not adjusted to container position on stage. 
+			scale += factorChange
+
+			//Determine the mouse pointer location in the map
+			let relativeX = centerX - this.container.x
+			let relativeY = centerY - this.container.y
+
+			//Determine the location in the map relative to entire map
+			let asPercentageX = relativeX / this.container.width
+			let asPercentageY = relativeY / this.container.height
+
+			console.log(scale)
+			//Scale map
+			this.container.scale.set(scale)
+
+			//Determine the X coordinate in map using the relative location
+			let newRelativeX = this.container.width * asPercentageX
+			let newRelativeY = this.container.height * asPercentageY
+
+			console.log(relativeX, relativeY)
+			console.log(newRelativeX, newRelativeY)
+
+			//Translate map by the difference in old coordinates under mouse and new coordinates under mouse.
+			translate(relativeX - newRelativeX, relativeY - newRelativeY)
+		}).bind(this)
+
 		var isDragging = false,
 		prevX, prevY;
 
-		this.container.on("mousedown", function (moveData) {
+		let isZooming = false
+
+		function down(moveData) {
 			var pos = moveData.data.global;
 			prevX = pos.x; prevY = pos.y;
 			isDragging = true;
-		});
+			console.log(moveData)
+			if (moveData.data.originalEvent?.touches?.length === 2) {
+			    isZooming = true;
+			}
+		}
 
-		this.container.on("mousemove", (function (moveData) {
+		this.container.on("mousedown", down);
+		this.container.on("touchstart", down)
+
+		let previousDist;
+		let startXAvg;
+		let startYAvg;
+
+		function pinchMove(e) {
+			var dist = Math.hypot(
+			    e.data.originalEvent.touches[0].pageX - e.data.originalEvent.touches[1].pageX,
+			    e.data.originalEvent.touches[0].pageY - e.data.originalEvent.touches[1].pageY);
+
+			if (!startXAvg && !startYAvg) {
+				startXAvg = (e.data.originalEvent.touches[0].pageX + e.data.originalEvent.touches[1].pageX)/2
+				startYAvg = (e.data.originalEvent.touches[0].pageY + e.data.originalEvent.touches[1].pageY)/2
+			}
+			if (previousDist) {
+				zoom((dist - previousDist) / 1000, startXAvg, startYAvg)
+			}
+			previousDist = dist
+		}
+
+		function move(moveData) {
 			if (!isDragging) {
 				return;
 			}
@@ -52,15 +115,36 @@ class Map {
 			var dx = pos.x - prevX;
 			var dy = pos.y - prevY;
 
-			//Drag and apply bounds. 
-			this.container.x = Math.min(Math.max(this.container.x + dx, this.bounds.xmin), this.bounds.xmax);
-			this.container.y = Math.min(Math.max(this.container.y + dy, this.bounds.ymin), this.bounds.ymax);;
-			prevX = pos.x; prevY = pos.y;
-		}).bind(this));
+			translate(dx, dy)
 
-		this.container.on("mouseup", function (moveData) {
+			prevX = pos.x; prevY = pos.y;
+
+			if (isZooming) {
+				pinchMove(moveData);
+			}
+		}
+
+		this.container.on("mousemove", move)
+		this.container.on("touchmove", move)
+
+		function end() {
 			isDragging = false;
-		});
+			isZooming = false;
+			previousDist = null;
+			startXAvg = null;
+			startYAvg = null;
+		}
+
+		this.container.on("mouseup", end)
+		this.container.on("touchend", end)
+
+		this.container.on("wheel", console.log)
+
+		document.body.addEventListener("wheel", (function(event){
+		    event.preventDefault()
+			console.log(event)
+			zoom(event.deltaY * -0.001, event.pageX, event.pageY)
+		}).bind(this), {passive: false});
 
 
 		this.items = config.items || []
